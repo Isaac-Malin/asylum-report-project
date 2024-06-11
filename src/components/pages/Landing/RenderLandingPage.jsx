a85a6804394b242d827b11f641792d0f20781fb5
Isaac Malin <Isaacmalin3530@gmail.com>
2024-06-11 15:10:47 -0600

    added 3 new images and styled them correctly

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // ADD IMPORTS BACK FOR GRAPHS SECTION
-// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
-// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
-// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
 import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
@@ -32,7 +32,21 @@ function RenderLandingPage(props) {
       </div>
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
-      {/* <div className="graphs-section"> */}
+      {/* added new img containers and styles to design page the user wanted */}
+      <div className="graphs-section">
+        <div className="grant-img-container">
+          <img src={GrantRatesByOfficeImg} alt="" />
+          <p>Search Grant Rates By Office</p>
+        </div>
+        <div className="grant-img-container">
+          <img src={GrantRatesByNationalityImg} alt="" />
+          <p>Search Grant Rates By Nationality</p>
+        </div>
+        <div className="grant-img-container">
+          <img src={GrantRatesOverTimeImg} alt="" />
+          <p>Search Grant Rates Over Time</p>
+        </div>
+      </div>
       <div className="view-more-data-btn-container">
         <Button
           type="default"
@@ -41,6 +55,14 @@ function RenderLandingPage(props) {
         >
           View the Data
         </Button>
+        <Button
+          type="default"
+          style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+          onClick={() => history.push('/graphs')}
+        >
+          Download the Data
+        </Button>{' '}
+        {/* added new button to download data */}
       </div>
 
       <div className="middle-section">
@@ -61,7 +83,9 @@ function RenderLandingPage(props) {
       </div>
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
-        {/* <div className="bottom-section">*/}
+        {/* adding system Disparity information */}
+
+        <div className="bottom-section"></div>
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
